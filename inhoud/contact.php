b6889f18411869179ed5c1de1b927c160fdb80cb
Lars399 <235134@student.scalda.nl>
2023-10-20 11:11:08 +0200

    begin van contact page, CSS nog niet werkend
    Begin van de contact page is gemaakt, CSS is nog niet werkende. PHP code voor tekst op te halen is der wel al, enkel nog in DB zetten

--- inhoud/contact.php
+++ inhoud/contact.php
@@ -1,1 +1,71 @@
-contact
+<div class="row">
+
+    <div class="jumbotron">
+        <p> Contact</p>
+    </div>
+
+</div>
+
+
+<div class="row">
+    <div class="col-sm-4"></div>
+
+    <div class="col-sm-4">
+        <h3 class="contacth3">Service & contact</h3>
+    </div>
+    <div class="col-sm-4"></div>
+
+</div>
+
+<div class="row">
+
+    <div class="col-sm-3"></div>
+
+    <div class="col-sm-6">
+       <div> <p class="telnummer">0118-855523</p>  <p class="email">Wijkcentrumdeburgerij@gmail.com</p> </div>
+    </div>
+
+    <div class="col-sm-3"></div>
+</div>
+
+<div class="row">
+
+    <div class="col-sm-2"></div>
+
+    <div class="col-sm-8">
+
+        <?php 
+
+             // Prepare the query
+        $query = "SELECT id, titel, tekst FROM contact";
+
+        // Execute the query
+        $result = $conn->query($query);
+
+        // Check if the query was successful
+        if ($result && $result->num_rows > 0) {
+            // Fetch the data from the result set
+            while ($row = $result->fetch_assoc()) {
+                $id = $row['id'];
+                $titel = $row['titel'];
+                $tekst = $row['tekst'];
+
+               
+                echo "<h1>", $title, "</h1>";
+                echo "<p>", $tekst, "</p>";
+               
+            }
+        } else {
+            echo "Der is geen tekst gevonden!";
+        }
+
+        // Close the result set
+        $result->close();
+        ?>
+
+    </div>
+
+    <div class="col-sm-2"></div> 
+
+
+</div>
