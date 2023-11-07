d3a10c57488ab13c6e567fa91c8c923377b68cb8
Lars399 <235134@student.scalda.nl>
2023-11-07 14:06:11 +0100

    404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,55 @@
-Ohhh nee, een 404!
+<head>
+    <title>de Burgerij - 404 </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--Booststrap4-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!--Own css-->
+    <link href="css/css.css" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/phone.css' rel='stylesheet'>
+
+</head>
+
+<?php include("header.php"); ?>
+
+<div class="row rowpadding404">
+
+    <div class="col-sm-12">
+
+        <h1 class="h1404">Oh nee, de pagina is niet gevonden!</h1>
+        <br><br><br>
+    </div>
+
+</div>
+
+<div class="row">
+
+    <div class="col-sm-3"></div>
+
+    <div class="col-sm-6">
+
+        <p class="p404">Beste bezoeker, Het lijkt erop dat de pagina waar u naar op zoek bent niet bestaat of fout gespeld is. <br><br>
+            Door <a class="a404" href="homepage">hier</a> te klikken gaat u terug naar onze home pagina <br>
+            Mocht dit blijven gebeuren kunt u het beste contact opnemen met ons. <br>
+            Telefoonnummer: 0118-855523
+            <br>
+            Email adres: Wijkcentrumdeburgerij@gmail.com <br><br>
+            Met vriendelijke groet, <br>
+            Wijkcentrum de Burgerij
+        </p>
+
+    </div>
+
+    <div class="col-sm-3"></div>
+
+</div>
+
+<?php include("footer.php"); ?>
