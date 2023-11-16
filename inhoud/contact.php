2cb509638a02b931e4145bd17e2d2f07823d677e
Lars399 <235134@student.scalda.nl>
2023-11-16 10:15:17 +0100

    contact laptop formaat responsive
    dis enkel laptop formaat (940x 1920)

--- inhoud/contact.php
+++ inhoud/contact.php
@@ -61,9 +61,9 @@
 
 <div class="row">
 
-    <div class="col-sm-3"></div>
+    <div class="col-sm-2"></div>
 
-    <div class="col-sm-7">
+    <div class="col-sm-8" >
 
         <?php
             echo "<h1 class=\"openingstijdentitel\">", $titel, "</h1>";
