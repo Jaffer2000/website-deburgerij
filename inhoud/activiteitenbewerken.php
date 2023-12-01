65c1ce3f1f0463cde53c929cd308ad3e67be4577
Lars <130559547+Lars399@users.noreply.github.com>
2023-12-01 07:58:59 +0100

    veranderd overzicht

--- inhoud/activiteitenbewerken.php
+++ inhoud/activiteitenbewerken.php
@@ -29,124 +29,61 @@
 
 </div>
 
-<div class="row overzichtopmaak">
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['verwijder'])) {
-        // Verwijderactie uitvoeren
-        $activiteitId = $_POST['activiteitId'];
-        $verwijderQuery = "DELETE FROM agenda WHERE id = $activiteitId";
 
-        if ($conn->query($verwijderQuery) === TRUE) {
-            echo '<script>alert("Activiteit verwijderd!");</script>';
-        } else {
-            echo '<script>alert("Fout bij het verwijderen van de activiteit: ' . $conn->error . '");</script>';
-        }
-    } else {
-        // Bijwerkactie uitvoeren
-        $activiteitId = $_POST['activiteitId'];
-        $huidigeFoto = $_POST['huidigeFoto'];
+<div class="row ">
+    <div class="col-sm-4"></div>
+    
+    <div class="col-sm-4 overzichtopmaak">
+        <?php
+                   // Prepare the select query
+        $selectQuery = "SELECT id, titel, tekst, foto, datum FROM agenda";
 
-        // Verkrijg de gegevens van het formulier
-        $nieuweTitel = $_POST['activiteitnaam'];
-        $nieuweDatum = $_POST['datum'];
-        $nieuweTekst = $_POST['bericht'];
+        // Execute the select query
+        $result = $conn->query($selectQuery);
+        
+        // Check if the query was successful
+        if ($result && $result->num_rows > 0) {
+            // Tabelkoppen
+            echo "<table border='1'>
+                    <tr>
+                        <th>Titel</th>
+                        <th>Datum</th>
+                        <th>Acties</th>
+                    </tr>";
 
-        // Upload nieuwe foto als deze is geselecteerd
-        $fotoNaam = '';
+            // Loop door de gegevens
+            while ($row = $result->fetch_assoc()) {
+                $id = $row['id'];
+                $titel = $row['titel'];
+                $tekst = $row['tekst'];
+                $foto = $row['foto'];
+                $datum = $row['datum'];
 
-        if ($_FILES['foto']['size'] > 0) {
-            $fotoMime = mime_content_type($_FILES['foto']['tmp_name']);
-
-            if ($fotoMime == 'image/jpeg' || $fotoMime == 'image/png' || $fotoMime == 'image/gif') {
-                // Het is een geldig afbeeldingstype, verwerk de upload
-                $fotoNaam = $_FILES['foto']['name'];
-                $fotoPad = 'img/' . $fotoNaam;
-
-                if (move_uploaded_file($_FILES['foto']['tmp_name'], $fotoPad)) {
-                    echo '<script>alert("Afbeelding succesvol geüpload.");</script>';
-                } else {
-                    echo '<script>alert("Fout bij het verplaatsen van het bestand.");</script>';
-                }
-            } else {
-                echo '<script>alert("Ongeldig bestandstype. Alleen JPG, PNG en GIF zijn toegestaan.");</script>';
+                // Tabelrijen
+                echo "<tr>
+                        <td>$titel</td>
+                        <td>$datum</td>
+                        <td>
+                            <a href='edit.php?id=$id'>Bewerken</a>
+                        </td>
+                        <td>
+                            <a href='delete.php?id=$id'>Verwijderen</a>
+                        </td>
+                      </tr>"; 
             }
+
+            // Tabel sluiten
+            echo "</table>";
         } else {
-            echo '<script>alert("Geen afbeelding geüpload.");</script>';
+            echo "Er is geen data gevonden!";
         }
 
-        // Controleer of $fotoNaam is ingesteld voordat je het gebruikt
-        if (!empty($fotoNaam)) {
-            // Voer de code uit die $fotoNaam gebruikt
-        } else {
-            // Doe iets anders of geef een foutmelding weer
-            echo '<script>alert("Fout bij het uploaden van de afbeelding.");</script>';
-        }
+        // Sluit de result set
+        $result->close();
 
-        // Voorbereid de update-query
-        $updateQuery = "UPDATE agenda SET titel = '$nieuweTitel', datum = '$nieuweDatum', tekst = '$nieuweTekst', foto = '$fotoNaam' WHERE id = $activiteitId";
+        ?>
+    </div>
 
-        // Voer de update-query uit
-        if ($conn->query($updateQuery) === TRUE) {
-            echo '<script>alert("Activiteit bijgewerkt!");</script>';
-        } else {
-            echo '<script>alert("Fout bijwerken activiteit: ' . $conn->error . '");</script>';
-        }
-    }
-}
-
-// Prepare the select query
-$selectQuery = "SELECT id, titel, tekst, foto, datum FROM agenda";
-
-// Execute the select query
-$result = $conn->query($selectQuery);
-
-// Check if the query was successful
-if ($result && $result->num_rows > 0) {
-    // Fetch the data from the result set
-    while ($row = $result->fetch_assoc()) {
-        $id = $row['id'];
-        $titel = $row['titel'];
-        $tekst = $row['tekst'];
-        $foto = $row['foto'];
-        $datum = $row['datum'];
-
-        // Formulier voor elke rij met col-sm-6
-        echo '
-        <div class="col-sm-6">
-            <form action="" method="post" enctype="multipart/form-data" class="formwidth">
-                <input type="hidden" name="activiteitId" value="' . $id . '">
-                <input type="hidden" name="huidigeFoto" value="' . $foto . '">
-
-                <label class="activiteitennaamform" for="activiteitnaam">Naam veranderen:</label> <br>
-                <input type="text" id="activiteitnaam" name="activiteitnaam" value="' . $titel . '" required> <br><br>
-
-                <label class="activiteitennaamform" for="datum">Datum veranderen:</label><br>
-                <input type="date" id="datum" name="datum" value="' . $datum . '" required> <br><br>
-
-                <label class="activiteitennaamform" for="bericht">Beschrijving veranderen:</label><br>
-                <textarea id="bericht" name="bericht" rows="4" required>' . $tekst . '</textarea> <br><br>
-
-
-                <label class="activiteitennaamform" for="foto">Huidige foto</label> <br>
-                <img src="img/' . $foto . '" alt="foto van activiteit" class="activiteitbewerkenimg"> <br><br>
-
-                <label class="activiteitennaamform" for="foto">Foto veranderen:</label><br>
-                <input type="file" id="foto" name="foto"/> <br><br>
-
-                <button class="activiteitentoevoegenbutton" type="submit">Bijwerken</button> <br><br><br>
-                <button class="verwijderbutton activiteitentoevoegenbutton" type="submit" name="verwijder">Verwijderen</button>
-            </form>
-        </div>
-        ';
-    }
-} else {
-    echo "Er is geen data gevonden!";
-}
-
-// Close the result set
-$result->close();
-?>
+    <div class="col-sm-4"></div>
 
 </div>
-
--- inhoud/nieuwsbewerken.php
+++ inhoud/nieuwsbewerken.php
@@ -28,130 +28,61 @@
     </div>
 
 </div>
+<div class="row ">
+    <div class="col-sm-4"></div>
+    
+    <div class="col-sm-4 overzichtopmaak">
+        <?php
+            // Prepare the select query
+            $selectQuery = "SELECT id, titel, tekst, foto, date_added FROM actueel";
 
-<div class="row">
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['verwijder'])) {
-        // Verwijderactie uitvoeren
-        $activiteitId = $_POST['activiteitId'];
-        $verwijderQuery = "DELETE FROM actueel WHERE id = $activiteitId";
+            // Execute the select query
+            $result = $conn->query($selectQuery);
+        
+            // Check if the query was successful
+            if ($result && $result->num_rows > 0) {
+                // Tabelkoppen
+                echo "<table border='1'>
+                        <tr>
+                            <th>Titel</th>
+                            <th>Datum</th>
+                            <th>Acties</th>
+                        </tr>";
 
-        if ($conn->query($verwijderQuery) === TRUE) {
-            echo '<script>alert("Activiteit verwijderd!");</script>';
-        } else {
-            echo '<script>alert("Fout bij het verwijderen van de activiteit: ' . $conn->error . '");</script>';
-        }
-    } else {
-        // Bijwerkactie uitvoeren
-        $activiteitId = $_POST['activiteitId'];
-        $huidigeFoto = $_POST['huidigeFoto'];
+                // Loop door de gegevens
+                while ($row = $result->fetch_assoc()) {
+                    $id = $row['id'];
+                    $titel = $row['titel'];
+                    $tekst = $row['tekst'];
+                    $foto = $row['foto'];
+                    $datum = $row['date_added'];
 
-        // Verkrijg de gegevens van het formulier
-        $nieuweTitel = $_POST['activiteitnaam'];
-        $nieuweDatum = $_POST['datum'];
-        $nieuweTekst = $_POST['bericht'];
-
-        // Upload nieuwe foto als deze is geselecteerd
-        $fotoNaam = '';
-
-        if ($_FILES['foto']['size'] > 0) {
-            $fotoMime = mime_content_type($_FILES['foto']['tmp_name']);
-
-            if ($fotoMime == 'image/jpeg' || $fotoMime == 'image/png' || $fotoMime == 'image/gif') {
-                // Het is een geldig afbeeldingstype, verwerk de upload
-                $fotoNaam = $_FILES['foto']['name'];
-                $fotoPad = 'img/' . $fotoNaam;
-
-                if (move_uploaded_file($_FILES['foto']['tmp_name'], $fotoPad)) {
-                    echo '<script>alert("Afbeelding succesvol geüpload.");</script>';
-                } else {
-                    echo '<script>alert("Fout bij het verplaatsen van het bestand.");</script>';
+                // Tabelrijen
+                echo "
+                    <tr>
+                        <td>$titel</td>
+                        <td>$datum</td>
+                        <td>
+                            <a href='edit.php?id=$id'>Bewerken</a>
+                        </td>
+                        <td>
+                            <a href='delete.php?id=$id'>Verwijderen</a>
+                        </td>
+                    </tr>"; 
                 }
+
+                // Tabel sluiten
+                echo "</table>";
             } else {
-                echo '<script>alert("Ongeldig bestandstype. Alleen JPG, PNG en GIF zijn toegestaan.");</script>';
+                echo "Er is geen data gevonden!";
             }
-        } else {
-            echo '<script>alert("Geen afbeelding geüpload.");</script>';
-        }
 
-        // Controleer of $fotoNaam is ingesteld voordat je het gebruikt
-        if (!empty($fotoNaam)) {
-            // Voer de code uit die $fotoNaam gebruikt
-        } else {
-            // Doe iets anders of geef een foutmelding weer
-            echo '<script>alert("Fout bij het uploaden van de afbeelding.");</script>';
-        }
+        // Sluit de result set
+        $result->close();
 
-        // Voorbereid de update-query
-        $updateQuery = "UPDATE actueel SET titel = '$nieuweTitel', date_added = '$nieuweDatum', tekst = '$nieuweTekst', foto = '$fotoNaam' WHERE id = $activiteitId";
+        ?>
+    </div>
 
-        // Voer de update-query uit
-        if ($conn->query($updateQuery) === TRUE) {
-            echo '<script>alert("Activiteit bijgewerkt!");</script>';
-        } else {
-            echo '<script>alert("Fout bijwerken activiteit: ' . $conn->error . '");</script>';
-        }
-    }
-}
-
-// Prepare the select query
-$selectQuery = "SELECT id, titel, tekst, foto, date_added FROM actueel";
-
-// Execute the select query
-$result = $conn->query($selectQuery);
-
-// Check if the query was successful
-if ($result && $result->num_rows > 0) {
-    // Fetch the data from the result set
-    while ($row = $result->fetch_assoc()) {
-        $id = $row['id'];
-        $titel = $row['titel'];
-        $tekst = $row['tekst'];
-        $foto = $row['foto'];
-        $datum = $row['date_added'];
-
-        $dateObject = new DateTime($datum);
-        $formattedDate = $dateObject->format('Y-m-d');
-
-        // Formulier voor elke rij met col-sm-6
-        echo '
-        <div class="col-sm-6 overzichtopmaak">
-            <form action="" method="post" enctype="multipart/form-data" class="formwidth">
-                <input type="hidden" name="activiteitId" value="' . $id . '">
-                <input type="hidden" name="huidigeFoto" value="' . $foto . '">
-
-                <label class="activiteitennaamform" for="activiteitnaam">Naam veranderen:</label> <br>
-                <input type="text" id="activiteitnaam" name="activiteitnaam" value="' . $titel . '" required> <br><br>
-
-                <label class="activiteitennaamform" for="datum">Datum veranderen:</label><br>
-                
-
-                <input type="date" id="datum" name="datum" value="' . $formattedDate . '" required> <br><br>
-
-                <label class="activiteitennaamform" for="bericht">Beschrijving veranderen:</label><br>
-                <textarea id="bericht" name="bericht" rows="4" required>' . $tekst . '</textarea> <br><br>
-
-
-                <label class="activiteitennaamform" for="foto">Huidige foto</label> <br>
-                <img src="img/' . $foto . '" alt="foto van activiteit" class="activiteitbewerkenimg"> <br><br>
-
-                <label class="activiteitennaamform" for="foto">Foto veranderen:</label><br>
-                <input type="file" id="foto" name="foto"/> <br><br>
-
-                <button class="activiteitentoevoegenbutton" type="submit">Bijwerken</button> <br><br><br>
-                <button class="verwijderbutton activiteitentoevoegenbutton" type="submit" name="verwijder">Verwijderen</button>
-            </form>
-        </div>
-        ';
-    }
-} else {
-    echo "Er is geen data gevonden!";
-}
-
-// Close the result set
-$result->close();
-?>
+    <div class="col-sm-4"></div>
 
 </div>
-
